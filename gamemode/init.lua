e7f9f4031a818dc7830957f8fc71ca878053c9d2
Jacob <jacob@z-tech.org>
2018-09-20 21:32:27 -0500

    player setup

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -3,3 +3,11 @@ DeriveGamemode("sandbox")
 
 AddCSLuaFile("shared.lua")
 AddCSLuaFile("cl_init.lua")
+
+AddCSLuaFile("shared.lua")
+
+function GM:PlayerInitialSpawn(client)
+    client:SetNoDraw(true)
+	client:SetNotSolid(true)
+    client:Lock()
+end
