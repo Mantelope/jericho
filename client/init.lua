03d8b27330b54a5c43bbdabd23d3e066aba1700a
Jacob <jacob@z-tech.org>
2018-09-20 19:31:58 -0500

    player setup

--- client/init.lua
+++ client/init.lua
@@ -1,5 +1,6 @@
 
 -- \brief Clear default garry's mod HUD.
-hook.Add("HUDShouldDraw", "HideHUD", function()
-    return false;
+
+hook.Add("HUDShouldDraw", "HideHUD", function(key)
+	return false;
 end)
