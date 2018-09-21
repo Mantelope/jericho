81cfde0e0410cac0e291b7707288730503af26db
Jacob <jacob@z-tech.org>
2018-09-21 16:00:13 -0500

    Main Menu UI

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -1,3 +1,3 @@
 
-jericho.include("core/derma/main_menu/menu_button.lua", jericho.shared)
-jericho.include("core/derma/main_menu/menu.lua", jericho.shared)
+jericho.include("core/derma/menu_button.lua", jericho.shared)
+jericho.include("core/derma/menu.lua", jericho.shared)
