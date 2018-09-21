23f0d7595daea8b968ef8b4152543147de17dffb
Jacob <jacob@z-tech.org>
2018-09-21 04:07:29 -0500

    Main Menu UI

--- gamemode/core/include.lua
+++ gamemode/core/include.lua
@@ -4,7 +4,7 @@
 --- \p path The path to the file to include.
 --- NOTE: This function can only be called in shared.lua
 function jericho.include(path)
-	assert(filename, "[ JERICHO ] jericho.include(filename): filename cannot be nil.");
+	assert(path, "[ JERICHO ] jericho.include(filename): filename cannot be nil.");
 
 	if SERVER then
 		include(path);
@@ -20,10 +20,10 @@ end
 --- NOTE: This function can only be called in shared.lua
 --- NOTE: This function will include all files in the directory given.
 function jericho.include_directory(dir)
-    assert(dir, "[ JERICHO ] jericho.include_directory(filename): filename cannot be nil.");
+    assert(dir, "[ JERICHO ] jericho.include_directory(filename): directory cannot be nil.");
 
 	for k, v in ipairs(file.Find("jericho/gamemode/" .. dir .. "/*.lua", "LUA")) do
         jericho.print("including \'" .. dir .. "/" .. v .. "\'")
-		nut.util.include(dir.."/"..v);
+		jericho.include(dir.."/"..v);
 	end
 end
