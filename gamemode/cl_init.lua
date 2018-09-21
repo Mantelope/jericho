a7e9f0eae0c55574bd98271167be1ca99f67bb98
Jacob <jacob@z-tech.org>
2018-09-20 22:33:06 -0500

    Main Menu UI

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -1,9 +1,9 @@
 
 kracken = kracken or {gui = {}}
 
+include("core/derma/main_menu.lua")
+
 -- \brief Clear default garry's mod HUD.
 hook.Add("HUDShouldDraw", "HideHUD", function(key)
 	return false;
 end)
-
-AddCSLuaFile("core/client/derma/main_menu.lua")
