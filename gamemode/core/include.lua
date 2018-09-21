fc39eb6e446dc5567fe3b18692ecd59ba4805c4e
Jacob <jacob@z-tech.org>
2018-09-21 15:54:54 -0500

    Main Menu UI

--- gamemode/core/include.lua
+++ gamemode/core/include.lua
@@ -9,14 +9,14 @@ function jericho.include(path, state)
 	jericho.assert(path, "jericho.include(path): path cannot be nil.");
 
 	if state == jericho.server then
-        jericho.print("including file \'" .. path .. "\' to server.");
+        jericho.print("attempting to include file \'" .. path .. "\' to server.");
         include(path);
     elseif state == jericho.shared then
-        jericho.print("including file \'" .. path .. "\' to shared.");
+        jericho.print("attempting to include file \'" .. path .. "\' to shared...");
 		AddCSLuaFile(path)
 		include(path)
     elseif state == jericho.client then
-        jericho.print("including file \'" .. path .. "\' to client.");
+        jericho.print("attempting to include file \'" .. path .. "\' to client.");
 
         if SERVER then
 			AddCSLuaFile(path)
--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -1,3 +1,3 @@
 
-jericho.include("derma/main_menu/menu_button.lua", jericho.shared)
-jericho.include("derma/main_menu/menu.lua", jericho.shared)
+jericho.include("core/derma/main_menu/menu_button.lua", jericho.shared)
+jericho.include("core/derma/main_menu/menu.lua", jericho.shared)
