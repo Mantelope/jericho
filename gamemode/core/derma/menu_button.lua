ea59e8bf64b60a4dca58ea4c28cb344ce59eea52
Jacob <jacob@z-tech.org>
2018-09-21 18:02:19 -0500

    Main Menu UI

--- gamemode/core/derma/menu_button.lua
+++ gamemode/core/derma/menu_button.lua
@@ -1,4 +1,7 @@
 
+--- \brief Button border radius.
+local radius = 5;
+
 local button_internal = {
     bg_color        = Color(0, 0, 0),           --- \brief The background color.
     shadow_color    = Color(236, 236, 236),     --- \brief The shadow color.
@@ -11,14 +14,13 @@ end
 
 function button_internal:Paint(w, h)
     --- \brief Draw button shadow.
-    draw.RoundedBox(self.radius, self.shadow_offset, self.shadow_offset, w, h, self.shadow_color);
+    draw.RoundedBox(radius, self.shadow_offset, self.shadow_offset, w, h, self.shadow_color);
 end
 
 vgui.Register("button_internal", button_internal, "DButton");
 
 local menu_button = {
     border_color    = Color(242, 196, 49),      --- \brief The border color.
-    radius          = 5,                        --- \brief Button border radius.
     padding         = 3,                        --- \brief Outline width.
 };
 
@@ -29,7 +31,7 @@ end
 
 function menu_button:Paint(w, h)
     if self:IsHovered() then
-        draw.RoundedBox(self.radius, 0, 0, w, h, self.border_color);
+        draw.RoundedBox(radius, 0, 0, w, h, self.border_color);
     end
 end
 
