0c790917105719097690cdacc161b88cf4185e6f
Jacob <jacob@z-tech.org>
2018-09-21 04:40:44 -0500

    Main Menu UI

--- gamemode/core/log.lua
+++ gamemode/core/log.lua
@@ -3,15 +3,9 @@ local header = "[ JERICHO ] "
 
 --- \brief Prints to output if jericho.debug is set to true.
 --- NOTE: jericho.debug is located in cl_init.lua.
-function jericho.print(...)
+function jericho.print(message)
     if jericho.debug then
-        local result = header;
-
-        for key, val in ipairs(arg) do
-            result = result .. tostring(val) .. "\t";
-        end
-
-        print(result .. "\n");
+        print(header .. message);
     end
 end
 
