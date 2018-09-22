1f14d919d620c1979fdacef5bca7edba41242e4d
Jacob <jacob@z-tech.org>
2018-09-21 20:47:27 -0500

    Main Menu UI

--- gamemode/core/derma/menu_button.lua
+++ gamemode/core/derma/menu_button.lua
@@ -3,7 +3,7 @@
 local blur = Material("pp/blurscreen")
 
 --- temp
-local ourMat = Material( "models/wireframe" )
+local ourMat = Material("overviews/de_inferno")
 
 local function DrawBlurRect(x, y, w, h)
     local X, Y = 0,0
@@ -43,14 +43,9 @@ end
 function button_internal:Paint(w, h)
     --- \brief Draw button shadow.
     draw.RoundedBox(radius, self.shadow_offset, self.shadow_offset, w, h, self.shadow_color);
-    surface.SetDrawColor( 255, 255, 255, 255 )
-	surface.SetMaterial(ourMat) -- If you use Material, cache it!
+    surface.SetDrawColor(255, 255, 255, 255)
+	surface.SetMaterial(ourMat)
 	surface.DrawTexturedRect( 0, 0, 512, 512 )
-
-    if self:IsHovered() then
-        local x, y = self:GetPos();
-        DrawBlurRect(x, y, w, h);
-    end
 end
 
 vgui.Register("button_internal", button_internal, "DImageButton");
